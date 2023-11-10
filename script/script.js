505612863830ca67d6d4774a3965ca4b1a07114d
Mike Johnson <mikjohns@fastenal.com>
2023-11-10 14:24:37 -0500

    Update script.js Book.info method

---
+++ script/script.js
@@ -0,0 +1,21 @@
+console.log("hello world");
+
+const myLibrary = [];
+
+function Book(title, author, pages, isBookRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isBookRead = Boolean(isBookRead);
+}
+
+Book.prototype.info = function () {
+  if (this.isBookRead === true) {
+    return `${this.title} by ${this.author}, ${this.pages} pages, book read.`;
+  }
+  return `${this.title} by ${this.author}, ${this.pages} pages, not read yet.`;
+};
+
+let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+
+function addBookToLibrary() {}
