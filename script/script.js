3962d293220fa1ff14e91a3c1612b2a0ab8cd0d4
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-11-13 08:18:54 -0500

    Add function to add book to library array

--- script/script.js
+++ script/script.js
@@ -17,5 +17,41 @@ Book.prototype.info = function () {
 };
 
 let theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+myLibrary.push(theHobbit);
+let greenEggsAndHam = new Book("Green Eggs and Ham", "Dr. Seuss", 32, true);
+myLibrary.push(greenEggsAndHam);
+let tuesdaysWith = new Book("Tuesdays with Morrie", "Mitch Albom", 144, true);
+myLibrary.push(tuesdaysWith);
 
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  // User Input to add a book to a library
+
+  let title = prompt("Title of the book?");
+  let author = prompt("Author?");
+  let pages;
+
+  while (isNaN(pages)) {
+    pages = prompt("How many pages?");
+  }
+  // Converts string pages to number
+  pages = +pages;
+
+  let bookRead = prompt("Have you read the book?", "Yes/No");
+  bookRead.toLowerCase();
+  bookRead === "yes" ? true : false;
+  const newBook = new Book(title, author, pages, bookRead);
+  myLibrary.push(newBook);
+}
+
+function displayBooks() {}
+
+let myFriends = [
+  {
+    name: "devin",
+    age: 25,
+  },
+  {
+    name: "odelys",
+    age: 25,
+  },
+];
