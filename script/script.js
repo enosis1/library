75b8748781333c61925eaf297fa91368f2beaec1
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-11-20 11:34:12 -0500

    Update modal styling

--- script/script.js
+++ script/script.js
@@ -1,16 +1,7 @@
 console.log("hello world");
 
-const body = document.querySelector("body");
-const bookContainer = document.querySelector(".book-container");
-const modal = document.querySelector("dialog");
-const addButton = document.querySelector(".addBookButton");
-const modalSubmitButton = document.querySelector(".formSubmitButton");
-const modalCloseButton = document.querySelector(".modalCloseButton");
-const formTitle = document.querySelector("#title");
-const formAuthor = document.querySelector("#author");
-const formPages = document.querySelector("#pages");
-const formCheck = document.querySelector("#isBookRead");
 const myLibrary = [];
+const modal = document.querySelector("dialog");
 
 function Book(title, author, pages, isBookRead) {
   this.title = title;
@@ -34,14 +25,14 @@ let tuesdaysWith = new Book("Tuesdays with Morrie", "Mitch Albom", 144, true);
 myLibrary.push(tuesdaysWith);
 
 function addBookToLibrary() {
-  // User Input to add a book to a library
-
   let title = formTitle.value;
   let author = formAuthor.value;
   let pages = formPages.value;
   let isBookRead = formCheck.value;
 
+  // Convert pages to a number
   pages = +pages;
+
   if (isBookRead === "true") {
     isBookRead = true;
   } else {
@@ -49,102 +40,6 @@ function addBookToLibrary() {
   }
   const newBook = new Book(title, author, pages, isBookRead);
   myLibrary.push(newBook);
-}
-
-let dataValue = 0;
-
-function displayBooks() {
-  myLibrary.forEach((book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-    const bookButtons = document.createElement("div");
-    const editButton = document.createElement("button");
-    const deleteButton = document.createElement("button");
-
-    for (let property in book) {
-      if (Object.hasOwn(book, property)) {
-        const bookInformation = document.createElement("div");
-        const bookLabel = document.createElement("p");
-        const bookInfo = document.createElement("p");
-        bookInformation.classList.add("book-information");
-        editButton.classList.add("button");
-        editButton.classList.add("edit-button");
-        deleteButton.classList.add("button");
-        deleteButton.classList.add("delete-button");
-        bookButtons.classList.add("book-buttons");
-
-        bookLabel.classList.add("book-label");
-        if (property === "isBookRead") {
-          bookLabel.textContent = `Book Read?:`;
-        } else {
-          bookLabel.textContent = `${capitalize(property)}:`;
-        }
-
-        bookInfo.classList.add("book-info");
-        bookInfo.textContent = `${book[property]}`;
-
-        editButton.textContent = "Edit";
-        deleteButton.textContent = "Delete";
-
-        bookInformation.appendChild(bookLabel);
-        bookInformation.appendChild(bookInfo);
-        bookCard.appendChild(bookInformation);
-        bookButtons.appendChild(editButton);
-        bookButtons.appendChild(deleteButton);
-        bookCard.appendChild(bookButtons);
-        bookCard.dataset.index = dataValue;
-      }
-    }
-
-    bookContainer.appendChild(bookCard);
-    dataValue++;
-  });
-}
-
-function displayBook(book) {
-  const bookCard = document.createElement("div");
-  bookCard.classList.add("book-card");
-  const bookButtons = document.createElement("div");
-  const editButton = document.createElement("button");
-  const deleteButton = document.createElement("button");
-
-  for (let property in book) {
-    if (Object.hasOwn(book, property)) {
-      const bookInformation = document.createElement("div");
-      const bookLabel = document.createElement("p");
-      const bookInfo = document.createElement("p");
-
-      bookInformation.classList.add("book-information");
-      editButton.classList.add("button");
-      deleteButton.classList.add("button");
-      bookButtons.classList.add("book-buttons");
-
-      bookLabel.classList.add("book-label");
-      if (property === "isBookRead") {
-        bookLabel.textContent = `Book Read?:`;
-      } else {
-        bookLabel.textContent = `${capitalize(property)}:`;
-      }
-
-      bookInfo.classList.add("book-info");
-      bookInfo.textContent = `${book[property]}`;
-
-      editButton.textContent = "Edit";
-      deleteButton.textContent = "Delete";
-
-      bookInformation.appendChild(bookLabel);
-      bookInformation.appendChild(bookInfo);
-      bookCard.appendChild(bookInformation);
-      bookButtons.appendChild(editButton);
-      bookButtons.appendChild(deleteButton);
-      bookCard.appendChild(bookButtons);
-      bookCard.dataset.index = dataValue;
-      book.index = dataValue;
-    }
-  }
-
-  bookContainer.appendChild(bookCard);
-  dataValue++;
 }
 
 function capitalize(string) {
@@ -159,44 +54,3 @@ function clearForm() {
 }
 
 displayBooks();
-
-addButton.addEventListener("click", () => {
-  clearForm();
-  modal.showModal();
-});
-
-modalCloseButton.addEventListener("click", () => {
-  modal.close();
-});
-
-modalSubmitButton.addEventListener("click", () => {
-  addBookToLibrary();
-  displayBook(myLibrary[myLibrary.length - 1]);
-});
-
-const deleteButton = document.querySelectorAll(".delete-button");
-const bookCards = document.querySelectorAll(".book-card");
-Array.from(deleteButton);
-const bookCards = document.querySelectorAll(".book-card");
-let bookCardsArray = Array.from(bookCards);
-
-deleteButton.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    let index = event.target.parentElement.parentElement.dataset.index;
-    bookCardsArray[index].remove();
-    myLibrary.splice(index, 1);
-    console.log(myLibrary);
-  });
-});
-// btn.addEventListener("click", (event) => {
-//   let index = event.target.parentElement.parentElement.dataset.index;
-//   index = +index;
-//   console.log(index);
-//   event.target.parentElement.parentElement.remove();
-
-//   for (let position in myLibrary) {
-//     if (myLibrary[position].index === index) {
-//       myLibrary.splice(position, 1);
-//     }
-//   }
-// });
