b6b8afe3b7e03cf34e31e89ce9c3ae8e2580aaec
Mike Johnson <72291166+enosis1@users.noreply.github.com>
2023-11-20 16:02:32 -0500

    Add functionality to delete button

--- script/script.js
+++ script/script.js
@@ -2,12 +2,17 @@ console.log("hello world");
 
 const myLibrary = [];
 const modal = document.querySelector("dialog");
+const addButton = document.querySelector(".addBookButton");
+const modalSubmitBtn = document.querySelector(".formSubmitButton");
+const modalCloseBtn = document.querySelector(".formCloseButton");
+const bookContainer = document.querySelector(".book-container");
 
 function Book(title, author, pages, isBookRead) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.isBookRead = Boolean(isBookRead);
+  this.randomNumber = Math.random();
 }
 
 Book.prototype.info = function () {
@@ -25,21 +30,152 @@ let tuesdaysWith = new Book("Tuesdays with Morrie", "Mitch Albom", 144, true);
 myLibrary.push(tuesdaysWith);
 
 function addBookToLibrary() {
+  const formTitle = document.querySelector("#title");
+  const formAuthor = document.querySelector("#author");
+  const formPages = document.querySelector("#pages");
+  const formBookRead = document.querySelector("#modalSelect");
+
   let title = formTitle.value;
   let author = formAuthor.value;
   let pages = formPages.value;
-  let isBookRead = formCheck.value;
+  let isBookRead = formBookRead.value;
 
-  // Convert pages to a number
-  pages = +pages;
+  isBookRead = isBookRead === "true" ? true : false;
 
-  if (isBookRead === "true") {
-    isBookRead = true;
-  } else {
-    isBookRead = false;
-  }
   const newBook = new Book(title, author, pages, isBookRead);
   myLibrary.push(newBook);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    const bookTitle = document.createElement("p");
+    bookTitle.classList.add("book-title");
+
+    const bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("book-author");
+
+    const bookPages = document.createElement("p");
+    bookPages.classList.add("book-pages");
+
+    const bookRead = document.createElement("p");
+    bookRead.classList.add("book-read");
+
+    const bookReadBtn = document.createElement("button");
+    bookReadBtn.setAttribute("type", "button");
+    bookReadBtn.classList.add("button", "read-button");
+    bookReadBtn.textContent = "Read?";
+
+    const bookDeleteBtn = document.createElement("button");
+    bookDeleteBtn.setAttribute("type", "button");
+    bookDeleteBtn.classList.add("button", "delete-button");
+    bookDeleteBtn.textContent = "Delete";
+
+    // Add dataset number;
+
+    bookDeleteBtn.addEventListener("click", (event) => {
+      event.target.parentElement.remove();
+      let numberImLookingFor = event.target.parentElement.dataset.number;
+      numberImLookingFor = +numberImLookingFor;
+      console.log(event.target.parentElement.dataset.number);
+      let indexIwillRemove = myLibrary.findIndex(
+        (book) => book.randomNumber === numberImLookingFor
+      );
+
+      myLibrary.splice(indexIwillRemove, 1);
+    });
+
+    bookReadBtn.addEventListener("click", () => {
+      let readStatus = bookRead.textContent;
+      readStatus.includes("true") ? console.log("true") : console.log("false");
+    });
+
+    for (let property in book) {
+      if (Object.hasOwn(book, property)) {
+        if (property === "title") {
+          bookTitle.textContent = book[property];
+        }
+        if (property === "author") {
+          bookAuthor.textContent = book[property];
+        }
+        if (property === "pages") {
+          bookPages.textContent = book[property];
+        }
+        if (property === "isBookRead") {
+          bookRead.textContent = `Book read?: ${book[property]}`;
+        }
+      }
+    }
+    bookContainer.appendChild(bookCard);
+    bookCard.appendChild(bookTitle);
+    bookCard.appendChild(bookAuthor);
+    bookCard.appendChild(bookPages);
+    bookCard.appendChild(bookRead);
+    bookCard.appendChild(bookReadBtn);
+    bookCard.appendChild(bookDeleteBtn);
+  });
+}
+
+function displayBook(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  const bookTitle = document.createElement("p");
+  bookTitle.classList.add("book-title");
+
+  const bookAuthor = document.createElement("p");
+  bookAuthor.classList.add("book-author");
+
+  const bookPages = document.createElement("p");
+  bookPages.classList.add("book-pages");
+
+  const bookReadBtn = document.createElement("button");
+  bookReadBtn.setAttribute("type", "button");
+  bookReadBtn.classList.add("button", "read-button");
+  bookReadBtn.textContent = "Read?";
+
+  const bookDeleteBtn = document.createElement("button");
+  bookDeleteBtn.setAttribute("type", "button");
+  bookDeleteBtn.classList.add("button", "delete-button");
+  bookDeleteBtn.textContent = "Delete";
+
+  bookDeleteBtn.addEventListener("click", (event) => {
+    event.target.parentElement.remove();
+    let numberImLookingFor = event.target.parentElement.dataset.number;
+    numberImLookingFor = +numberImLookingFor;
+    console.log(event.target.parentElement.dataset.number);
+    let indexIwillRemove = myLibrary.findIndex(
+      (book) => book.randomNumber === numberImLookingFor
+    );
+    myLibrary.splice(indexIwillRemove, 1);
+  });
+
+  for (let property in book) {
+    if (Object.hasOwn(book, property)) {
+      if (property === "title") {
+        bookTitle.textContent = book[property];
+      }
+      if (property === "author") {
+        bookAuthor.textContent = book[property];
+      }
+      if (property === "pages") {
+        bookPages.textContent = book[property];
+      }
+      if (property === "isBookRead") {
+        if (book[property] === true) {
+        } else {
+        }
+      }
+    }
+  }
+  bookContainer.appendChild(bookCard);
+  bookCard.appendChild(bookTitle);
+  bookCard.appendChild(bookAuthor);
+  bookCard.appendChild(bookPages);
+  bookCard.appendChild(bookReadBtn);
+  bookCard.appendChild(bookDeleteBtn);
 }
 
 function capitalize(string) {
@@ -47,10 +183,29 @@ function capitalize(string) {
 }
 
 function clearForm() {
+  const formTitle = document.querySelector("#title");
+  const formAuthor = document.querySelector("#author");
+  const formPages = document.querySelector("#pages");
+  const formBookRead = document.querySelector("#modalSelect");
+
   formTitle.value = "";
   formAuthor.value = "";
   formPages.value = "";
-  formCheck.value = "";
+  formBookRead.value = "";
 }
+
+addButton.addEventListener("click", () => {
+  modal.showModal();
+});
+
+modalSubmitBtn.addEventListener("click", () => {
+  addBookToLibrary();
+  displayBook(myLibrary[myLibrary.length - 1]);
+});
+
+modalCloseBtn.addEventListener("click", () => {
+  clearForm();
+  modal.close();
+});
 
 displayBooks();
